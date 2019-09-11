52b94ee0faa7827c683303ceb72668430649db47
duchop98 <hopnd98@gmail.com>
2019-09-11 11:57:25 +0700

    Finish layout and routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,10 @@ class StaticPagesController < ApplicationController
 
   def aboutme
   end
+
+  def about
+  end
+
+  def contact
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,21 +2,33 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Home | Hop Blog"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | Hop Blog"
   end
 
   test "should get aboutme" do
-    get static_pages_aboutme_url
+    get aboutme_path
     assert_response :success
     assert_select "title", "About Me | Hop Blog"
+  end
+
+  test "should get about" do
+    get about_path
+    assert_response :success
+    assert_select "title", "About | Hop Blog"
+  end
+
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | Hop Blog"
   end
 
 end
