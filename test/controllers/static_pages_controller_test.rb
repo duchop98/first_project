7c7b330fa21d5faedad03e43fb7d6103b3bb0168
duchop98 <hopnd98@gmail.com>
2019-09-11 09:54:38 +0700

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Hop Blog"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Hop Blog"
   end
 
   test "should get aboutme" do
     get static_pages_aboutme_url
     assert_response :success
+    assert_select "title", "About Me | Hop Blog"
   end
 
 end
